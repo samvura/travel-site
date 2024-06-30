0a220d0e194b52e0fedaebd8d4df47b62093fa9f
Samvura <samvura.mpofu@gmail.com>
2024-06-30 14:34:21 +0100

    Completed the modal overlay.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,11 +3,27 @@ import MobileMenu from './Modules/MobileMenu'
 import RevealOnScroll from './Modules/RevealOnScroll'
 import StickyHeader from './Modules/StickyHeader'
 
-let stickyHerder = new StickyHeader()
+
+
+new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+new MobileMenu();
+let modal
 
-let mobileMenu = new MobileMenu();
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault()
+        if (typeof modal == "undefined") {
+            import(/* webpackChunkName: "modal" */'./Modules/Modal').then(x => {
+                modal = new x.default()
+                setTimeout(() => modal.openTheModal(), 20)
+            }).catch(() => console.log("There was a problem!"))
+        } else {
+            modal.openTheModal()
+        }
+    })
+})
 
 if (module.hot) {
   module.hot.accept()
